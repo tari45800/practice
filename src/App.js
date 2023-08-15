ed9bf203df6266c6e58393c601022d989daae4c6
tari45800 <tari45800@gmail.com>
2023-08-15 22:28:47 +0900

    feat: 리액트 기초2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import ReviewList from "./components/ReviewList";
-import items from "./mock.json";
+import mockItems from "./mock.json";
 
 function App() {
+  const [items, setItems] = useState(mockItems);
+  const [order, setOrder] = useState(`createdAt`);
+  const sortedItem = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder(`createdAt`);
+  const handleBestClick = () => setOrder(`rating`);
+  const handleDelete = (id) => {
+    const nestItem = items.filter((item) => item.id !== id);
+    setItems(nestItem);
+  };
+
   return (
     <div>
-      <ReviewList items={items} />
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>평점순</button>
+      </div>
+      <ReviewList items={sortedItem} onDelete={handleDelete} />
     </div>
   );
 }
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -5,7 +5,9 @@ function formatData(value) {
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()} `;
 }
 
-function ReviewListItem({ item }) {
+function ReviewListItem({ item, onDelete }) {
+  const handleDeleteClick = () => onDelete(item.id);
+
   return (
     <ReviewListItemDiv>
       <img src={item.imgUrl} alt={item.title}></img>
@@ -14,16 +16,21 @@ function ReviewListItem({ item }) {
         <p>{item.rating}</p>
         <p>{formatData(item.createdAt)}</p>
         <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>삭제</button>
       </div>
     </ReviewListItemDiv>
   );
 }
 
-function ReviewList({ items }) {
+function ReviewList({ items, onDelete }) {
   return (
     <ul>
-      {items.map((el, idx) => (
-        <ReviewListItem item={el} key={idx} />
+      {/* map에서 key를 지정해주는 이유는 해당 el의 순서가 바뀌어도 데이터를 유지하기 위함입니다.
+          idx값은 고유한 값이 아니며 해당 el의 순서가 바뀌면 idx도 따라서 바뀝니다.
+          따라서 idx는 key값으로 사용하기 부적절합니다. 
+      */}
+      {items.map((el) => (
+        <ReviewListItem item={el} key={el.id} onDelete={onDelete} />
       ))}
     </ul>
   );
